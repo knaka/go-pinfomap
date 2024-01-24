6c3df1c7002655e6a157160da8a1ba7a5bf3d9d2
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-01-24 17:50:10 +0900

    Add methods analysis

--- work/bar.go
+++ work/bar.go
@@ -1,32 +1,15 @@
 package main
 
-import "log"
-
-type Bar struct {
-	N int
-}
+type Bar struct{}
 
 func (rcv *Bar) GreetRef(name string) string {
-	rcv.N++
 	return "hello" + name
 }
 
 func (rcv Bar) Greet() (int, error) {
-	rcv.N++
 	return 123, nil
 }
 
-func bar() {
-	bar := Bar{}
-	log.Println(bar.N)
-	bar.Greet()
-	log.Println(bar.N)
-	bar.GreetRef("foo")
-	log.Println(bar.N)
-	barRef := &Bar{}
-	log.Println(barRef.N)
-	barRef.Greet()
-	log.Println(barRef.N)
-	barRef.GreetRef("foo")
-	log.Println(barRef.N)
+type Baz struct {
+	bar Bar
 }

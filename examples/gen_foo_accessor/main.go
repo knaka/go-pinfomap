0936908718f51ea869713af95b3ccdaa4ee1a9ea
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-01-25 14:29:23 +0900

    Introduce any

--- examples/gen_foo_accessor/main.go
+++ examples/gen_foo_accessor/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"github.com/knaka/pinfomap"
-	examples "github.com/knaka/pinfomap/examples"
-	"reflect"
+	"github.com/knaka/pinfomap/examples"
 )
 
 func main() {
 	structInfo, err := pinfomap.GetStructInfo(
-		reflect.TypeOf(examples.Foo{}).PkgPath(),
-		reflect.TypeOf(examples.Foo{}).Name(),
+		examples.Foo{},
 		&pinfomap.GetStructInfoParams{
 			Tags: []string{"accessor"},
 			Data: map[string]any{
 				"AdditionalComment": "This is a comment.",
 			},
-		})
+		},
+	)
 	if err != nil {
 		panic(err)
 	}
-	err = pinfomap.Generate(pinfomap.AccessorsTemplate, structInfo)
+	err = pinfomap.Generate(pinfomap.AccessorTemplate, structInfo)
 	if err != nil {
 		panic(err)
 	}
